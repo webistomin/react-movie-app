06653fdc805a637973b098caf043ad61cb891e4c
webistomin <webistomin@gmail.com>
2020-02-01 17:10:00 +0300

    feat(card): правки карточки

--- src/components/ui/AppCard/AppCard.js
+++ src/components/ui/AppCard/AppCard.js
@@ -3,12 +3,16 @@ import './AppCard.sass';
 import { Link } from 'react-router-dom';
 import AppLazyImage from '../AppLazyImage';
 import AppRating from '../AppRating';
+import AppIcon from '../AppIcon';
 
 const AppCard = () => {
   return (
     <article className='card'>
       <Link to='/' className='card__link'>
         <figure className='card__figure'>
+          <button type='button' className='card__like'>
+            <AppIcon className='icon_heart' icon='icon-heart' width='24' height='24' />
+          </button>
           <picture className='card__picture'>
             <AppLazyImage className='card__img' />
           </picture>

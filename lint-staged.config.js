1a42368b0c3177a579eee6b755b6277206d5ef41
webistomin <webistomin@gmail.com>
2020-03-18 18:16:48 +0300

    feat(lint): добавил линтер для скриптов

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  'src/**/*.{js,jsx,json,md}': ['npm run lint:write', 'npm run prettier:write', 'npm run ec:write', 'git add'],
+  'src/**/*.{js,jsx,json,md}': [
+    'npm run lint:write',
+    'npm run prettier:write',
+    'npm run ec:write',
+    'npx scriptlint',
+    'git add',
+  ],
 };
---
+++ scriptlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  strict: false,
+  fix: true,
+  config: false,
+  json: false,
+  rules: {},
+  ignoreScripts: [],
+  customRules: [],
+};

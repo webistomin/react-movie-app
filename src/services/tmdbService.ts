2ab65446e7ea5ac4380e3926797f61c5d843aaab
webistomin <webistomin@gmail.com>
2020-04-25 19:19:47 +0300

    refactor(service): удалил лишний await

--- src/services/tmdbService.ts
+++ src/services/tmdbService.ts
@@ -20,87 +20,87 @@ export default class TMDb {
       throw new Error(`Could not fetch ${url}, received ${response.status} error`);
     }
 
-    return await response.json();
+    return response.json();
   };
 
   public getGenres = async (): Promise<IGenre[]> => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}genre/movie/list?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}`
     );
   };
 
   public getContentBySearchQuery = async (query: string, page = 1) => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}search/movie?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}&query=${query}&page=${page}&include_adult=false`
     );
   };
 
   public getMovieDetails = async (id: number): Promise<IMovieDetails> => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}movie/${id}?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}`
     );
   };
 
   public getMovieImages = async (id: number): Promise<IImages> => {
-    return await this.fetchJSON(`${this.API_BASE}movie/${id}/images?api_key=${this.TMDB_API_KEY}`);
+    return this.fetchJSON(`${this.API_BASE}movie/${id}/images?api_key=${this.TMDB_API_KEY}`);
   };
 
   public getMovieRecommendations = async (id: number): Promise<ISearch<IMovie>> => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}movie/${id}/recommendations?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}`
     );
   };
 
   public getMovieSimilar = async (id: number, page = 1): Promise<ISearch<IMovie>> => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}movie/${id}/similar?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}&page=${page}`
     );
   };
 
   public getLatestMovie = async (): Promise<IMovie> => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}movie/latest?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}&include_adult=false`
     );
   };
 
   public getNowPlayingMovies = async (page = 1): Promise<ISearch<IMovie>> => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}movie/now_playing?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}&page=${page}`
     );
   };
 
   public getPopularMovies = async (page = 1): Promise<ISearch<IMovie>> => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}movie/popular?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}&page=${page}`
     );
   };
 
   public getTopRatedMovies = async (page = 1): Promise<ISearch<IMovie>> => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}movie/top_rated?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}&page=${page}`
     );
   };
 
   public getUpcomingMovies = async (page = 1): Promise<ISearch<IMovie>> => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}movie/upcoming?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}&page=${page}`
     );
   };
 
   public getMovieCredits = async (id: number): Promise<IMovieCredits> => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}movie/${id}/credits?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}`
     );
   };
 
   public getMovieVideos = async (id: number): Promise<IVideos> => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}movie/${id}/videos?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}`
     );
   };
 
   public getPersonDetails = async (id: number): Promise<IPersonDetails> => {
-    return await this.fetchJSON(
+    return this.fetchJSON(
       `${this.API_BASE}person/${id}?api_key=${this.TMDB_API_KEY}&language=${this.API_LANGUAGE}&append_to_response=movie_credits`
     );
   };
